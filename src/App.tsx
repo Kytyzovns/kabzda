5a644382b03b6bf37fb610b9f6c1e25b22930334
mykytos <kytyzovns@gmail.com>
2024-05-10 15:18:36 +0300

    first kabzda homework

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
+/*import logo from './logo.svg';*/
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+      <div>
+        <AppTitle/>
+        <Rating/>
+      </div>
   );
 }
+
+function Star() {
+  return (
+      <div>Star</div>
+  );
+}
+
+function Rating() {
+  return (<>
+    <Star/>
+    <Star/>
+    <Star/>
+    <Star/>
+    <Star/>
+  </>);
+}
+
+function AppTitle() {
+  return <>This is APP component</>
+}
+
+/*function Accordion {
+
+}*/
 
 export default App;
