660bbed8fcc600a33829c8ed6ca0c52832b4cb8f
mykytos <kytyzovns@gmail.com>
2024-05-10 15:33:02 +0300

    second kabzda homework

--- src/App.tsx
+++ src/App.tsx
@@ -3,36 +3,59 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-      <div>
-        <AppTitle/>
-        <Rating/>
-      </div>
-  );
+    return (
+        <div>
+            <AppTitle/>
+            <Rating/>
+            <Accordion/>
+            <Rating/>
+        </div>
+    );
 }
 
 function Star() {
-  return (
-      <div>Star</div>
-  );
+    console.log("star rendered")
+    return (
+        <div>Star</div>
+    );
 }
 
 function Rating() {
-  return (<>
-    <Star/>
-    <Star/>
-    <Star/>
-    <Star/>
-    <Star/>
-  </>);
+    console.log("rating rendered")
+    return <>
+        <Star/>
+        <Star/>
+        <Star/>
+        <Star/>
+        <Star/>
+    </>
 }
 
 function AppTitle() {
-  return <>This is APP component</>
+    return <>This is APP component</>
 }
 
-/*function Accordion {
+function Accordion() {
+    console.log("accordion rendered");
 
-}*/
+    return <div>
+        <AccordionTitle/>
+        <AccordionBody/>
+    </div>
+}
+
+function AccordionTitle() {
+    console.log("accordionTitle rendered");
+    return <h3>Menu</h3>
+}
+
+function AccordionBody() {
+    console.log("accordionBody rendered");
+    return <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+    </ul>
+}
 
 export default App;
